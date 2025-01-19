ae8e90d650969f658815acd55e640b29e86c4949
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-01-19 09:08:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "remote" {
+    organization = "your-terraform-cloud-organization"
+
+    workspaces {
+      name = "azure-resource-group"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+  tags = {
+    Environment = var.environment
+  }
+}
