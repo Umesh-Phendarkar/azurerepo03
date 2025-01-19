256c22ca321bd034b351e3ea977e549601ad96f7
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-01-19 13:49:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = "resource_group01"
+  location = "East US"
   tags = {
-    Environment = var.environment
+    Environment = Dev
   }
 }
